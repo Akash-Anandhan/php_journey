cc57fec8ccf62b72a60d3fe5200245cac3583f17
Akash-Anandhan <akashasha.as@gmail.com>
2025-04-27 13:27:39 +0530

    minor change in code orientation

--- index.php
+++ index.php
@@ -16,6 +16,26 @@ $s_data = [
         'age' => 18,
         'gender' => "female",]
 ];
+/*this block is how to write a funnction in php
+in this case ive written a funtioon to filter the s_daata array based on the gender */
+function filterbygender($array, $gender)
+{
+    $FilteredArray = [];
+    foreach ($array as $item) {
+        if ($item['gender'] === $gender) {
+            $FilteredArray[] = $item;
+
+        }
+
+    }
+    return $FilteredArray;
+}
+
+/*created for genric funtions and learnt to seperate the php file form the html for redudant code*/
+$filterdata = array_filter($s_data, function ($data) {
+    return $data['age'] < 20;
+});
+
  require "index.view.php";
 
 
--- index.view.php
+++ index.view.php
@@ -33,25 +33,8 @@
     } ?>
     only <?= $students[0]; ?> is a naughty boy
 </p>
-<!--this block is how to write a funnction in php
-in this case ive written a funtioon to filter the s_daata array based on the gender -->
-    <?php
-    function filterbygender($array, $gender){
-        $FilteredArray = [];
-        foreach($array as $item){
-            if($item['gender'] === $gender){
-                $FilteredArray[] = $item;
 
-            }
 
-        }
-        return $FilteredArray;
-    }
-    /*created for genric funtions and learnt to seperate the php file form the html for redudant code*/
-    $filterdata = array_filter($s_data, function($data){
-        return $data['age']< 20 ;
-    })
-    ?>
 <ul>
     <?php foreach($filterdata as $data) : ?>
         <li>
